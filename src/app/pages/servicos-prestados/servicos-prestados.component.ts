11fec4a35d950cd0e39ebb2d548e1dc699d725f2
Dener Troquatte <you@example.com>
2023-11-21 19:44:12 -0300

    Curso de Angular

--- src/app/pages/servicos-prestados/servicos-prestados.component.ts
+++ src/app/pages/servicos-prestados/servicos-prestados.component.ts
@@ -7,7 +7,7 @@ import {
   inject,
   signal,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servicos-prestados',
@@ -19,6 +19,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export default class ServicosPrestadosComponent implements OnInit {
   #route = inject(ActivatedRoute);
+  #router = inject(Router);
 
   public getId = signal<null | string>(null);
   @Input() set id(id: string) {
@@ -28,5 +29,17 @@ export default class ServicosPrestadosComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.#route.snapshot.params['id']);
     this.#route.params.subscribe((res) => console.log(res['id']));
+
+    console.log(this.#route.snapshot.queryParamMap.get('name'));
+    console.log(this.#route.snapshot.queryParamMap.get('age'));
+
+    this.#route.queryParamMap.subscribe({
+      next: (next) => {
+        console.log(next.get('name'));
+        console.log(next.get('age'));
+      },
+    });
+
+    setTimeout(() => this.#router.navigate(['/curso']), 3000);
   }
 }
