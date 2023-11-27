d488cdfffff0ddd3a274b9cce5014a10048d6111
Dener Troquatte <you@example.com>
2023-11-27 14:44:36 -0300

    Curso de Angular

--- src/app/components/animations/animations.component.ts
+++ src/app/components/animations/animations.component.ts
@@ -2,7 +2,7 @@ import {
   animate,
   keyframes,
   query,
-  sequence,
+  stagger,
   state,
   style,
   transition,
@@ -81,34 +81,61 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
             background: 'yellow',
             transform: 'translateY(100px)',
           }),
-          // stagger('700ms', [animate('1s')]),
-          sequence([
-            animate(
-              '1s',
-              style({
-                background: 'red',
-              })
-            ),
-            animate(
-              '2s',
-              style({
-                background: 'blue',
-              })
-            ),
-            animate(
-              '7s',
-              style({
-                background: 'none',
-                transform: 'translateY(0)',
-              })
-            ),
-            animate(
-              '1s',
-              style({
-                transform: 'translateY(100px)',
-              })
-            ),
-          ]),
+          stagger('700ms', [animate('1s')]),
+          // group([
+          //   animate(
+          //     '1s',
+          //     style({
+          //       background: 'red',
+          //     })
+          //   ),
+          //   animate(
+          //     '2s',
+          //     style({
+          //       background: 'blue',
+          //     })
+          //   ),
+          //   animate(
+          //     '7s',
+          //     style({
+          //       background: 'none',
+          //       transform: 'translateY(0)',
+          //     })
+          //   ),
+          //   animate(
+          //     '1s',
+          //     style({
+          //       transform: 'translateY(100px)',
+          //     })
+          //   ),
+          // ]),
+          // sequence([
+          //   animate(
+          //     '1s',
+          //     style({
+          //       background: 'red',
+          //     })
+          //   ),
+          //   animate(
+          //     '2s',
+          //     style({
+          //       background: 'blue',
+          //     })
+          //   ),
+          //   animate(
+          //     '7s',
+          //     style({
+          //       background: 'none',
+          //       transform: 'translateY(0)',
+          //     })
+          //   ),
+          //   animate(
+          //     '1s',
+          //     style({
+          //       transform: 'translateY(100px)',
+          //     })
+          //   ),
+          // ]),
         ]),
       ]),
     ]),
